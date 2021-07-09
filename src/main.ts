5f1a18f9e0d4ffc1cd2d24ef355ead422fbaee78
A. Sauder <andre.sauder@netzwerkcenter.ch>
2021-07-09 18:57:20 +0200

    add workshops.de banner on startup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log('                       _          _                               _\n' +
+  '                      | |        | |                             | |\n' +
+  '__      __ ___   _ __ | | __ ___ | |__    ___   _ __   ___     __| |  ___\n' +
+  '\\ \\ /\\ / // _ \\ | \'__|| |/ // __|| \'_ \\  / _ \\ | \'_ \\ / __|   / _` | / _ \\\n' +
+  ' \\ V  V /| (_) || |   |   < \\__ \\| | | || (_) || |_) |\\__ \\ _| (_| ||  __/\n' +
+  '  \\_/\\_/  \\___/ |_|   |_|\\_\\|___/|_| |_| \\___/ | .__/ |___/(_)\\__,_| \\___|\n' +
+  '                                               | |\n' +
+  '                                               |_|')
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
